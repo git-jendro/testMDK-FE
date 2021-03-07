470d047e1dba7011edbdca562012536272e1fe0f
git-jendro <jendro.hard@gmail.com>
2021-03-07 15:56:07 +0700

    'deploy'

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,8 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ public_path('js/app.js') }}"></script>
+    {{-- <script src="{{ public_path('js/jquery.min.js') }}"></script> --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">
