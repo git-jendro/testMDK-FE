faee1e6e8b156d151b6a5eec9da2e0b4b996e79a
git-jendro <jendro.hard@gmail.com>
2021-03-07 15:51:20 +0700

    'deploy'

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -8,8 +8,9 @@
     <title>List User</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="{{ public_path('js/app.js') }}"></script>
-    <script src="{{ public_path('js/jquery.min.js') }}"></script>
-    <script src="{{ public_path('js/moments.js') }}"></script>
+    {{-- <script src="{{ public_path('js/jquery.min.js') }}"></script> --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment-with-locales.min.js"></script>
 </head>
 <body>
     <div class="container">
